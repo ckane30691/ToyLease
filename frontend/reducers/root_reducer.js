f49453af7916145b3611426c4a66e3697665f41c
Cory Kane <ckane30691@gmail.com>
2018-01-29 12:57:10 -0800

    Get redux cycle set up for userAuth

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import entities from './entities_reducer';
-// import session from './session_reducer';
+import session from './session_reducer';
 import errors from './errors_reducer';
 
 const RootReducer = combineReducers({
-  entities,
-  // session,
+  // entities,
+  session,
   errors
 });
 
---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,35 @@
+export const login = user => (
+  $.ajax({
+    method: 'POST',
+    url: 'api/session',
+    data: 'user'
+  })
+);
+
+export const logout = () => (
+  $.ajax({
+    method: 'DELETE',
+    url: 'api/session',
+  })
+);
+
+export const signup = user => (
+  $.ajax({
+    method: 'POST',
+    url: 'api/users',
+    data: user
+  })
+);
+
+export const dummyLogin = () => (
+  $.ajax({
+    method: 'POST',
+    url: 'api/session',
+    data: {
+      user: {
+      username: "Dwight",
+      password: "password"
+      }
+    }
+  })
+);
