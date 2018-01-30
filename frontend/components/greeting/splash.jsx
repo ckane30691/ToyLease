c1428cced44bbc1123dcedf29cb71466ae47a28b
Cory Kane <ckane30691@gmail.com>
2018-01-29 16:02:10 -0800

    Finally get hero image the way I want lol

--- frontend/components/greeting/splash.jsx
+++ frontend/components/greeting/splash.jsx
@@ -4,7 +4,16 @@ import SessionFormContainer from '../session_form/session_form_container';
 const Splash = () => (
   <div className='splash'>
 		<div className="hero-img-container">
-      SPLASH SPLASH SPLASH SPLISH
+      <img className="hero-img" alt="hero image"
+        src="https://s3.amazonaws.com/toy-lease/mly.jpg"/>
+    </div>
+
+    <div className="intro-container">
+
+			<p className="intro-body">
+				Everybody wishes they had more toys and now is your chance to
+        play with every toy without breaking the bank.
+			</p>
     </div>
   </div>
 );
