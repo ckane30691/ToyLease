2d6fd7cf29a060b1ebadc19f1b9be5f8ad26eebf
Cory Kane <ckane30691@gmail.com>
2018-01-30 19:51:18 -0800

    Write controller for leasings

--- app/controllers/api/leasings_controller.rb
+++ app/controllers/api/leasings_controller.rb
@@ -1,2 +1,32 @@
 class Api::LeasingsController < ApplicationController
+  def index
+    @leasing = current_user.leasings.includes(:toy)
+    render :index
+  end
+
+  def create
+    @leasing = current_user.leasings.new(leasing_params)
+    toy = Toy.find_by(id: @leasing.toy_id)
+    if @leasing.leaser_id == toy.owner_id
+      render json: ['This is your own toy!'], status: 422
+    elsif @leasing.save
+      render :show
+    else
+      render json: @leasing.errors.full_messages, status: 422
+    end
+
+  end
+
+  def destroy
+    @leasing = Leasing.find_by(id: params[:id])
+    @leasing.destroy
+    render :show
+  end
+
+  private
+
+  def leasing_params
+    params.require(:leasing).permit(:start_date, :end_date,
+                                    :leaser_id, :toy_id)
+  end
 end
